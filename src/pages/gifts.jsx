587651850380cd297ca37911fcb4d26b4d2ad4f3
Blair Nangle <hi@blairnangle.com>
2022-08-02 11:52:34 +0100

    Update gifts page to mention honeymoon contributions

--- src/pages/gifts.jsx
+++ src/pages/gifts.jsx
@@ -9,7 +9,8 @@ function GiftsPage() {
         Gifts are absolutely not necessary! But, if you did want to, we have a wedding list
         {' '}
         <a href="https://www.weddingshop.com/Buy/View/111746">here</a>
-        .
+        —or if you did wish to contribute to our honeymoon or future home,
+        money would be hugely appreciated!
       </p>
     </Layout>
   );
